bcf57e6bb9ca2bab4bca7a97d67a2e54e654477a
takechi-single <t.takaaki0906@gmail.com>
2020-10-17 16:53:25 +0900

    Revert "Revert "既読機能機能のフロントエンド実装""
    This reverts commit 1e308a58df0d2112049cfeba6418585e89e55778.

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2020_08_24_033624) do
 
-  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
     t.boolean "checked"
     t.datetime "created_at", precision: 6, null: false
